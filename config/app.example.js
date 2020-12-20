ebdb121c83f9b957c799a4dd01226f71f7ca893e
nhi101298 <nhiphan.24hdev@gmail.com>
2020-12-21 01:35:20 +0700

    Apply .gitignore

--- config/app.example.js
+++ config/app.example.js
@@ -9,9 +9,7 @@ module.exports = {
     refresh_token_expires_in: "365d",
   },
   paypal: {
-    clientId:
-      "AR1N8XTZdI-fGyfBCT_6ZjLLzd4pO0P2a9jGagARJRiKriPPMH7H9bQx_YHY_3pSwozQwVqXxhKWd_SK",
-    clientSecret:
-      "EN_P5yuijIJYQk6S3doJV30Xqkv2TJtwyDQIFOBfBRLCrTiKFmv7V05C9WWlI-YXKWxqz1FivHfxAvwX",
+    clientId: "",
+    clientSecret: "",
   },
 };
